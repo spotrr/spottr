e56bdc24d249b8a051d3b94e35fde8dd41ce9cc5
ronke <rsoyekunle@gmail.com>
2021-07-30 20:19:21 -0400

    Revert "Redux boilerplate done"

--- client/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import reducers from './reducers/index';
-
-// create the store with dev tools
-const store = createStore(reducers, composeWithDevTools());
-
-export default store;
